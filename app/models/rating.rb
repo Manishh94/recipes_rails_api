945c455e8b138ddd94c68388ee04969ff203ce4d
Manish Prajapat <manishprajapat94@gmail.com>
2022-09-19 23:18:27 +0530

    added validation to the rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,7 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :resource, polymorphic: true
+
+  validates :rate, length: { maximum: 255, minimum: 0, message: I18n.t('.out_of_range_error') }, presence: true
+  validates :comment, length: { maximum: 65_535, minimum: 0, message: I18n.t('.out_of_range_error') }
 end
